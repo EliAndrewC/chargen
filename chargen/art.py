c7e04d4e906a7986954b51384993659a29e02169
Eli Courtwright <eli@courtwright.org>
2026-01-24 23:53:59 -0500

    added art stuff

---
+++ chargen/art.py
@@ -0,0 +1,14 @@
+# This file will eventually be used to generate art.  The prompts I use when
+# generating prompts usually looks something like this:
+
+"""
+The PCs are talking to a samurai from the <clan name goes here> clan.
+
+-> <he / she> is in <his / her> early <decade of age, e.g. '30s' or '20s', etc>
+
+-> she is wearing <clan name> clan colors of <clan colors - see CLAN_COLORS in constants.py>
+
+-> <I usually try to include some kind of detail relevant to who this NPC is, what traits they have, their position, their personality, etc>
+
+Make a colored, photo-realistic, life-like rendering, matching the Legend of the Five Rings setting of Rokugan, based on Edo-period Japan with period-appropriate clothing and possessions being shown.  The background must be completely blank without any features.
+"""
